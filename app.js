39b801b6509f983343c75ffa9d727a2bc7dbd7fc
wbc <417780894@qq.com>
2020-04-05 14:55:14 +0800

    优化

--- app.js
+++ app.js
@@ -20,17 +20,11 @@ var app = express();
 
 const ENV = process.env.NODE_ENV
 if (ENV !== 'production') {
-    // 开发环境 / 测试环境
-    app.use(logger('dev'));
-} else {
-    // 线上环境
-    const logFileName = path.join(__dirname, 'logs', 'access.log')
-    const writeStream = fs.createWriteStream(logFileName, {
-        flags: 'a'
-    })
-    app.use(logger('combined', {
-        stream: writeStream
-    }));
+  app.use(logger('dev')); // 开发环境 / 测试环境
+} else { // 线上环境
+  const logFileName = path.join(__dirname, 'logs', 'access.log')
+  const writeStream = fs.createWriteStream(logFileName, { flags: 'a' })
+  app.use(logger('combined', { stream: writeStream }));
 }
 
 app.use(express.json());
@@ -39,17 +33,15 @@ app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
 const redisClient = require('./db/redis')
-const sessionStore = new RedisStore({
-    client: redisClient
-})
+const sessionStore = new RedisStore({ client: redisClient })
 app.use(session({
-    secret: 'WJiol#23123_',
-    cookie: {
-        // path: '/',   // 默认配置
-        // httpOnly: true,  // 默认配置
-        maxAge: 24 * 60 * 60 * 1000
-    },
-    store: sessionStore
+  secret: 'WJiol#23123_',
+  cookie: {
+    // path: '/',   // 默认配置
+    // httpOnly: true,  // 默认配置
+    maxAge: 24 * 60 * 60 * 1000
+  },
+  store: sessionStore
 }))
 
 // app.use('/', indexRouter);
@@ -58,19 +50,19 @@ app.use('/api/blog', blogRouter);
 app.use('/api/user', userRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    next(createError(404));
+app.use(function (req, res, next) {
+  next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'dev' ? err : {};
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'dev' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
--- routes/user.js
+++ routes/user.js
@@ -3,25 +3,19 @@ var router = express.Router();
 const { login } = require('../controller/user')
 const { SuccessModel, ErrorModel } = require('../model/resModel')
 
-router.post('/login', function(req, res, next) {
-    const { username, password } = req.body
-	const result = login(username, password)
-	
-    return result.then(data => {
-        if (data.username) {
-            // 设置 session
-            req.session.username = data.username
-            req.session.realname = data.realname
+router.post('/login', function (req, res, next) {
+  const { username, password } = req.body
+  const result = login(username, password)
 
-            res.json(
-                new SuccessModel()
-            )
-            return
-        }
-        res.json(
-            new ErrorModel('登录失败')
-        )
-    })
+  return result.then(data => {
+    if (data.username) {
+      req.session.username = data.username   // 设置 session
+      req.session.realname = data.realname
+      res.json(new SuccessModel())
+      return
+    }
+    res.json(new ErrorModel('登录失败'))
+  })
 });
 
 // router.get('/login-test', (req, res, next) => {
